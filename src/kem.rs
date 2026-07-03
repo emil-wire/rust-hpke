c69565972891fe15fe8df0828892a31f5ce764c2
Michael Rosenberg <michael@mrosenberg.pub>
2026-07-03 19:35:39 -0400

    Address GenerateKeyPair mismatch

--- src/kem.rs
+++ src/kem.rs
@@ -87,6 +87,17 @@ pub trait Kem: Sized {
     }
 
     /// Generates a random keypair using the given RNG
+    // Implementation note: This simply does DeriveKeyPair(random(Nsk)). This does not
+    // match the definition of GenerateKeyPair in the PQ and and XWing implementations
+    //   <https://www.ietf.org/archive/id/draft-ietf-hpke-pq-04.html#section-3-7>
+    // and
+    //   <https://www.ietf.org/archive/id/draft-connolly-cfrg-xwing-kem-10.html#section-5.2-2>
+    // In reality, though, we don't care. The goal of this function is to produce a random
+    // keypair, and it guarantees no cross-compatiblity. There are no test vectors for it,
+    // and the original RFC doesn't even define it (merely uses "can be defined")
+    //   <https://datatracker.ietf.org/doc/html/rfc9180#section-4-7>
+    // Thus, for us it suffices to just keep this definition in place. More discussion here
+    //   <https://mailarchive.ietf.org/arch/msg/hpke/v1Jw382gXveSQxsi8RE4S9ep-bY/>
     fn gen_keypair_with_rng(csprng: &mut impl CryptoRng) -> (Self::PrivateKey, Self::PublicKey) {
         // Make some keying material that's the size of a private key
         let mut ikm: Array<u8, <Self::PrivateKey as Serializable>::OutputSize> = Array::default();
